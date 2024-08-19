43055df4ca4e2df07e09b047d2f1637822c7b9a8
JatinRM <jatinmahakal12@gmail.com>
2024-08-20 00:00:33 +0530

    fix error

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./Song-Lyrics-Finder-App/",
+  base: "./Song-Lyrics-Finder-App",
   plugins: [react()],
 })
